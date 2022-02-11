53b7d668203be52dfaea58c9d8c8b1c8c50a6557
Cphe <cpheboss@hotmail.com>
2022-02-11 13:57:20 +0100

    day7-part2 solution

--- 7/solve.hs
+++ 7/solve.hs
@@ -4,6 +4,10 @@ import Data.List.Split
 import System.Environment                                                                                               
                                                                                                                         
 import Debug.Trace                                                                                                      
+
+conso from to
+  | from > to = conso to from
+  | otherwise = ((to-from)*(to-from+1)) `div` 2
                                                                                                                         
 main = do                                                                                                               
     file:args <- getArgs                                                                                                
@@ -20,3 +24,7 @@ main = do
     let result1 = minimum $ map sum $ map (\n -> map (\x -> abs (x-n)) crabPos) [(minimum crabPos)..(maximum crabPos)]
     putStrLn "result1 ="
     print result1
+
+    let result2 = minimum $ map sum $ map (\n -> map (\x -> conso x n) crabPos) [(minimum crabPos)..(maximum crabPos)]
+    putStrLn "result2 ="
+    print result2
