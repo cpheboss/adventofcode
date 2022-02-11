855619e7637dce07879cfe09f77c4bbf45c2a8c8
Cphe <cpheboss@hotmail.com>
2022-02-11 17:47:33 +0100

    day8-part1 solution

--- 8/solve.hs
+++ 8/solve.hs
@@ -12,3 +12,18 @@ main = do
     putStrLn "line = "                                                                                                  
     print line                                                                                                          
 
+    let splitLines = map ((splitAt 10) . words . filter (/='|')) $ lines line
+    putStrLn "splitLines ="
+    print splitLines
+
+    let patterns = map fst splitLines
+    let digits = map snd splitLines
+
+    putStrLn "patterns = "
+    print patterns
+    putStrLn "digits = "
+    print digits
+
+    let result1 = sum $ map ( length . filter (\s -> length s `elem` [2,3,4,7])) digits
+    putStrLn "result1 = "
+    print result1
