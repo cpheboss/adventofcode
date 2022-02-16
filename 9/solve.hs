62204d40b8c61a03d073bd3d64e9cdbbe428cd3d
Cphe <cpheboss@hotmail.com>
2022-02-16 08:46:06 +0100

    day9-part1 solution

--- 9/solve.hs
+++ 9/solve.hs
@@ -1,4 +1,16 @@
-import System.Environment
+import Data.List.Split
+import           Data.Char(digitToInt)
+import           Data.List
+import qualified Data.Vector as V
+import           System.Environment
+
+compare3 :: [Int] -> [Int]
+compare3 (a:b:[] ) = []
+compare3 (a:b:c:s) = [compA + compC] ++ compare3 (b:c:s)
+  where compA = if a <= b then 1 else 0
+        compC = if c <= b then 1 else 0
+
+surround9 l = [9] ++ l ++ [9]
 
 main = do
     file:args <- getArgs
@@ -7,3 +19,17 @@ main = do
     putStrLn "line = "
     print line
 
+    let rawLines = lines line
+
+    let intLines = map (map digitToInt) rawLines
+    putStrLn "intLines ="
+    mapM_ print $ intLines
+
+    let depthsByWidth = map (compare3 . surround9) intLines
+
+    let depthsByHeight = map (compare3 . surround9) $ transpose intLines
+
+    let lowerNeighbours = map (\(l1,l2) -> zipWith (+) l1 l2) $ zip depthsByWidth $ transpose depthsByHeight
+
+    putStrLn "result 1 ="
+    print $ sum $ zipWith (\x y -> if y > 0 then 0 else x+1) (concat intLines) (concat lowerNeighbours)
