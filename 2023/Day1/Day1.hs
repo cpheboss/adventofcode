78d366d497ac6588949074a55059214d2a5cafd4
Bibi <cpheboss@hotmail.com>
2023-12-21 08:20:05 +0100

    Day1 Part1

--- 2023/Day1/Day1.hs
+++ 2023/Day1/Day1.hs
@@ -1,11 +1,26 @@
 module Day1.Day1 where
 
 import System.Environment
+import Text.Regex.TDFA
+import Debug.Trace
 
-main = do
-    file : args <- getArgs
-    putStrLn $ "Reading " ++ file
-    line <- readFile file
-    putStrLn "line = "
-    print line
+getDigits :: String -> [String]
+getDigits s = getAllTextMatches (s =~ "[0-9]") :: [String]
+
+toInt :: String -> Int
+toInt = read::String->Int
+
+joinFirstAndLast :: [String] -> String
+joinFirstAndLast [] = "0"
+joinFirstAndLast x = head x ++ last x
+
+result1 :: [String] -> String
+result1 (lines) = do
+    let stringDigits = map getDigits lines
+    let intStrings = map joinFirstAndLast stringDigits
+    let ints = map toInt intStrings
+    let res = sum ints
+    traceShow res ""
+
+    show res
 
--- 2023/Main.hs
+++ 2023/Main.hs
@@ -1,6 +1,14 @@
 module Main where
 
 import Day1.Day1
+import System.Environment
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    file : args <- getArgs
+    putStrLn $ "Reading file " ++ file
+    file_content <- readFile file
+--    putStrLn file_content
+    let ll = lines file_content
+    putStrLn "Result:"
+    putStrLn $ Day1.Day1.result1 ll
